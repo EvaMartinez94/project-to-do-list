92106f228367b6c9084d80fefe12a129bc94a931
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-06-03 15:17:40 +0200

    update task

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,15 +1,32 @@
 package org.example;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public final class App {
-    
-    private ArrayList <String> list1 = new ArrayList<>();
-    private ArrayList <Boolean> list2 = new ArrayList<>();
+
+    private ArrayList<String> list1 = new ArrayList<>();
+    private ArrayList<Boolean> list2 = new ArrayList<>();
     private int taskNumber = 0;
     private Scanner sc = new Scanner(System.in);
-    private boolean selectMenuOption = true; 
-    private boolean addNewTask = true;
-    
-    public static void main(String[] args) {}
+    private boolean selectMenuOption = true;
+
+
+    public void updateTasks() {
+        printStaticMessage();
+        int taskNumber = sc.nextInt();
+        boolean taskCompleted;
+        list2.set(taskNumber-1, true);
+        System.out.println("Modified ArrayList: " + list2);
+        System.out.println("Your task has been modified");
+    }
+
+    public static void printStaticMessage() {
+        System.out.println("What task do you want to mark as completed? Enter the number of task: ");
+    }
+    //readTasks();
+    public static void main(String[] args) {
+        App app = new App();
+        app.updateTasks();
+    }
 }
