51b4e83c3e4ac5ed20d95144da71441a08113d46
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-06-03 09:36:41 +0200

    add variables

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public final class App {
+    
+    private ArrayList <String> list1 = new ArrayList<>();
+    private ArrayList <Boolean> list2 = new ArrayList<>();
+    private int taskNumber = 0;
+    private Scanner sc = new Scanner(System.in);
+    private boolean selectMenuOption = true; 
+    private boolean addNewTask = true;
+    
+    public static void main(String[] args) {}
+}
