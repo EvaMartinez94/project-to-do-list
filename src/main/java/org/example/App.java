2fcbf5400ab628d6ce183f9ecb2853234485c286
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-06-03 15:29:01 +0200

    add read feature

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,5 +11,22 @@ public final class App {
     private boolean selectMenuOption = true; 
     private boolean addNewTask = true;
     
+    public void readTasks() {
+        System.out.println("This is your current Task List: ");
+        
+        for (int i=0; i<list1.size();i++) {
+            boolean crossedTask = list2.get(i);
+            String crossed = "";
+            if( crossedTask == true)
+                {
+                crossed = ".[X]";
+                }
+            else 
+                {
+                crossed = ".[ ]";
+                }
+            System.out.println((i+1)+crossed+list1.get(i));
+            }
+    }
     public static void main(String[] args) {}
 }
